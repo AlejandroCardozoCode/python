fc7f792faad9f218f22533271ee2fba403d5ed55
srmcfly <d.alejandro198@gmail.com>
2021-03-18 19:25:39 -0500

    1-0

---
+++ nombre.py
@@ -0,0 +1,4 @@
+nombre = "Alejandro"
+edad = "22"
+
+print("hola " + nombre + " tu edad es de " + edad)
---
+++ numbers.py
@@ -0,0 +1,9 @@
+numero = 5
+numero2 = -3.90
+resultado = numero / numero2
+print(numero)
+print("%.2f" % resultado)
+
+#para imprimir como string se hace de la siguiente manera
+
+print("el numero es: " + str(numero))
